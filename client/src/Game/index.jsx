e90b4c6e5d13c6919235bea238734d6ad053034a
Pákozdi György <gyorgypakozdi@gmail.com>
2022-12-22 13:01:24 +0100

    feat(client): initial benchmark page (#27)
    Co-authored-by: gyuri <gyuri@gyuris-MacBook-Air.local>

--- client/src/Game/index.jsx
+++ client/src/Game/index.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Stage, Sprite } from '@inlet/react-pixi'
+import { Stage, Sprite } from '@inlet/react-pixi';
 import Control from './Control';
 import './index.css';
 
-const resize = (canvasRef, cWidth, cHeight)  => {
+const resize = (canvasRef, cWidth, cHeight) => {
   const { width, height } = document.body.getBoundingClientRect();
-  const zoomLevel = Math.min(width/cWidth, height/cHeight);
-  canvasRef.current._canvas.style.zoom = zoomLevel
+  const zoomLevel = Math.min(width / cWidth, height / cHeight);
+  canvasRef.current._canvas.style.zoom = zoomLevel;
 };
 
 const Game = function ({ id }) {
@@ -17,6 +17,7 @@ const Game = function ({ id }) {
   useEffect(() => {
     window.starx.on('GameUpdate', (data) => {
       setPlayers(data.objects);
+      console.log(data)
     });
   }, []);
 
@@ -28,16 +29,16 @@ const Game = function ({ id }) {
     doResize();
   }, [canvasRef]);
 
-  const image = "image6.png"
+  const image = 'image6.png';
 
   return (
-   <Control>
-      <Stage ref={canvasRef} width={WIDTH} height={HEIGHT} >  
-      {players.map(p => (
-        <Sprite image={image} x={p.x} y={p.y} />
-      ))}
-    </Stage>
-   </Control>
+    <Control>
+      <Stage ref={canvasRef} width={WIDTH} height={HEIGHT}>
+        {players.map((p, i) => (
+          <Sprite image={image} key={`player_${i}`} x={p.x} y={p.y} />
+        ))}
+      </Stage>
+    </Control>
   );
 };
 
